71a9566276e997d1ec0ba673d2e0263f2b7a44af
Augusto <59477015+cesarempathy@users.noreply.github.com>
2023-01-23 23:07:01 +0100

    Add sentry (#17)
    * add sentry integration
    
    * clean code
    
    * clean code
    
    Co-authored-by: Eco Punk <madacluster@madaclusters-MacBook-Pro.local>

---
+++ src/hooks.server.ts
@@ -0,0 +1,15 @@
+import { Toucan } from 'toucan-js';
+import type { HandleServerError } from '@sveltejs/kit';
+import { SENTRY_DSN } from '$env/static/private'; 
+export const handleError = (({ error, event }) => {
+  const sentry = new Toucan({
+    dsn: SENTRY_DSN,
+    release: '1.0.0',
+  });
+  const errorId = sentry.captureException(error);
+ 
+  return {
+    message: 'Whoops!',
+    errorId: errorId
+  };
+}) satisfies HandleServerError;
