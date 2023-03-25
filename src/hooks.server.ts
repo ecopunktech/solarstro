96a390b55672e674c96bdf2f11cd0c235b768d48
ecopunk <59477015+cesarempathy@users.noreply.github.com>
2023-03-25 18:00:27 +0100

    add sentry integration

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,15 +2,15 @@ import { Toucan } from 'toucan-js';
 import type { HandleServerError } from '@sveltejs/kit';
 import { SENTRY_DSN } from '$env/static/private'; 
 export const handleError = (({ error, event }) => {
-  // const sentry = new Toucan({
-  //   dsn: SENTRY_DSN,
-  //   release: '1.0.0',
-  // });
+  const sentry = new Toucan({
+    dsn: SENTRY_DSN,
+    release: '1.0.0',
+  });
   console.log('error', error);
-  // const errorId = sentry.captureException(error);
+  const errorId = sentry.captureException(error);
  
   return {
     message: 'Whoops!',
-    errorId: "errorId"
+    errorId: errorId
   };
 }) satisfies HandleServerError;
