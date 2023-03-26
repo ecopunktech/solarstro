3df1194b24a66ba6f8dfa48b2e299bc9e3f6b0d6
ecopunk <59477015+cesarempathy@users.noreply.github.com>
2023-03-26 18:06:25 +0200

    add redis cache

--- backend/main.go
+++ backend/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/go-redis/redis/v8"
 )
 
 func main() {
+	// get env redis
+	redisURI := os.Getenv("REDIS_URL")
+	opt, _ := redis.ParseURL(redisURI)
+	redisClient := redis.NewClient(opt)
+
 	// GetSolarInfo()
-	http.HandleFunc("/svg", svgHandler)
-	http.HandleFunc("/report", rectanglesHandler)
+	http.HandleFunc("/svg", svgHandler(redisClient))
+	http.HandleFunc("/report", reportHandler(redisClient))
 
 	fmt.Println("Server listening on :8080")
 
@@ -17,37 +25,42 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func TestMain() {
+// func TestMain() {
 
-	rc := "33034A02000029"
-	cadastralCode := "33034A02000041"
-	coord, err := getCoordinatesFromCadastralCode(cadastralCode)
+// 	rc := "33034A02000029"
+// 	cadastralCode := "33034A02000041"
+// 	coord, err := getCoordinatesFromCadastralCode(cadastralCode)
 
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	lon := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Geo.Xcen
-	lat := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Geo.Ycen
-	address := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Ldt
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 		return
+// 	}
+// 	lon := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Geo.Xcen
+// 	lat := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Geo.Ycen
+// 	address := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Ldt
 
-	fmt.Printf("Coordinates for cadastral code %s: X: %s, Y: %s\nAdrress: %s\n", rc, lon, lat, address)
+// 	fmt.Printf("Coordinates for cadastral code %s: X: %s, Y: %s\nAdrress: %s\n", rc, lon, lat, address)
+// 	redisClient := redis.NewClient(&redis.Options{
+// 		Addr:     "localhost:6379", // Redis server address
+// 		Password: "",               // Redis password
+// 		DB:       0,                // Redis database number
+// 	})
 
-	gmlFile, err := getGMLFromCatastro(rc)
-	if err != nil {
-		fmt.Println(err)
-	}
-	polygon, err := getCoordinatesFromGML(gmlFile)
-	if err != nil {
-		fmt.Println(err)
-	}
-	area := polygonArea(polygon)
-	panasonic := PanasonicSolarPanel()
+// 	gmlFile, err := getGMLFromCatastro(rc, redisClient)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	polygon, err := getCoordinatesFromGML(gmlFile)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	area := polygonArea(polygon)
+// 	panasonic := PanasonicSolarPanel()
 
-	panels := rectanglesInsidePolygon(polygon, Rectangle{Width: panasonic.Width / 1000, Height: panasonic.Height / 1000})
+// 	panels := rectanglesInsidePolygon(polygon, Rectangle{Width: panasonic.Width / 1000, Height: panasonic.Height / 1000})
 
-	fmt.Println("Panels:", len(panels), "Area:", area, "m2")
-	fmt.Println("Panasonic:")
-	GetSolarInfo(lat, lon, len(panels), panasonic)
+// 	fmt.Println("Panels:", len(panels), "Area:", area, "m2")
+// 	fmt.Println("Panasonic:")
+// 	GetSolarInfo(lat, lon, len(panels), panasonic)
 
-}
+// }
--- backend/web.go
+++ backend/web.go
@@ -5,124 +5,146 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+
+	"github.com/go-redis/redis/v8"
 )
 
-func svgHandler(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
-	rc := query.Get("rc")
-	if rc == "" {
-		http.Error(w, "Missing 'rc' parameter", http.StatusBadRequest)
-		return
-	}
+func svgHandler(redisClient *redis.Client) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		rc := query.Get("rc")
+		if rc == "" {
+			http.Error(w, "Missing 'rc' parameter", http.StatusBadRequest)
+			return
+		}
 
-	gmlFile, err := getGMLFromCatastro(rc)
-	if err != nil {
-		fmt.Println(err)
-	}
-	polygon, err := getCoordinatesFromGML(gmlFile)
-	if err != nil {
-		fmt.Println(err)
-	}
+		gmlFile, err := getGMLFromCatastro(rc, redisClient)
+		if err != nil {
+			fmt.Println(err)
+		}
+		polygon, err := getCoordinatesFromGML(gmlFile)
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	minX, minY, maxX, maxY := PoligonBB(polygon)
+		minX, minY, maxX, maxY := PoligonBB(polygon)
 
-	minPoint := Point{minX, minY}
-	maxPoint := Point{maxX, maxY}
-	panels := rectanglesInsidePolygon(polygon, Rectangle{Width: 2, Height: 2})
+		minPoint := Point{minX, minY}
+		maxPoint := Point{maxX, maxY}
+		panels := rectanglesInsidePolygon(polygon, Rectangle{Width: 2, Height: 2})
 
-	// scaleFactor := 10.0
-	scaledPolygon := scaleCoordinates(polygon, 1000, 1000)
-	rectangule := make([]Rectangle, len(panels))
-	for i, p := range panels {
-		rectangule[i] = scaleRectangle(p, minPoint, maxPoint, 1000, 1000)
+		// scaleFactor := 10.0
+		scaledPolygon := scaleCoordinates(polygon, 1000, 1000)
+		rectangule := make([]Rectangle, len(panels))
+		for i, p := range panels {
+			rectangule[i] = scaleRectangle(p, minPoint, maxPoint, 1000, 1000)
+		}
+		svgData, err := createSVG(scaledPolygon, rectangule)
+		if err != nil {
+			fmt.Println("Error creating SVG data:", err)
+			return
+		}
+		w.Header().Set("Content-Type", "image/svg+xml")
+		w.Write(svgData)
 	}
-	svgData, err := createSVG(scaledPolygon, rectangule)
-	if err != nil {
-		fmt.Println("Error creating SVG data:", err)
-		return
-	}
-	w.Header().Set("Content-Type", "image/svg+xml")
-	w.Write(svgData)
 }
+func reportHandler(redisClient *redis.Client) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		rc := query.Get("rc")
+		if rc == "" {
+			http.Error(w, "Missing 'rc' parameter", http.StatusBadRequest)
+			return
+		}
+		areaPercentageQuery := query.Get("percentage")
+		if areaPercentageQuery == "" {
+			areaPercentageQuery = "100"
+		}
 
-func rectanglesHandler(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
-	rc := query.Get("rc")
-	if rc == "" {
-		http.Error(w, "Missing 'rc' parameter", http.StatusBadRequest)
-		return
+		areaPercentage, err := strconv.ParseFloat(areaPercentageQuery, 64)
+		if err != nil && areaPercentage < 0 && areaPercentage > 100 {
+			http.Error(w, "Invalid 'area_percentage' parameter", http.StatusBadRequest)
+			return
+		}
+
+		budget := query.Get("budget")
+		if budget == "" {
+			budget = "med"
+		}
+		resultsChan := make(chan [2]interface{}, 2)
+		go func() {
+			coord, err := getCoordinatesFromCadastralCode(rc, redisClient)
+			resultsChan <- [2]interface{}{coord, err}
+		}()
+
+		go func() {
+			gml, err := getGMLFromCatastro(rc, redisClient)
+			resultsChan <- [2]interface{}{gml, err}
+		}()
+		// wait for the two functions to complete and receive their results
+		var coord CatastroResponse
+		var gmlFile string
+		for i := 0; i < 2; i++ {
+			result := <-resultsChan
+			if result[1] != nil {
+				fmt.Println("Error:", result[1])
+				http.Error(w, "Error getting data", http.StatusBadRequest)
+				return
+			}
+			switch v := result[0].(type) {
+			case CatastroResponse:
+				coord = v
+			case string:
+				gmlFile = v
+			default:
+				fmt.Println("Unknown result type:", v)
+				http.Error(w, "Error getting data", http.StatusBadRequest)
+				return
+			}
+		}
+
+		lon := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Geo.Xcen
+		lat := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Geo.Ycen
+		address := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Ldt
+
+		polygon, err := getCoordinatesFromGML(gmlFile)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, "Error getting coordinates from GML", http.StatusBadRequest)
+			return
+		}
+		area := polygonArea(polygon)
+		solarPanelType := GetSolarPanelByBudget(budget)
+
+		panelsTotal := rectanglesInsidePolygon(polygon, Rectangle{Width: solarPanelType.Width / 1000, Height: solarPanelType.Height / 1000})
+		panels := panelsTotal[:int(float64(len(panelsTotal))*areaPercentage/100)]
+
+		solarFarmResponse, err := GetSolarInfo(lat, lon, len(panels), solarPanelType)
+		if err != nil {
+			fmt.Println("Error getting solar info:", err)
+			http.Error(w, "Error getting solar info", http.StatusInternalServerError)
+			return
+		}
+
+		response := struct {
+			CadastralCode string            `json:"cadastral_code"`
+			Latitude      string            `json:"latitude"`
+			Longitude     string            `json:"longitude"`
+			Address       string            `json:"address"`
+			Area          float64           `json:"area"`
+			NumPanels     int               `json:"num_panels"`
+			SolarInfo     SolarFarmResponse `json:"solar_info"`
+		}{
+			CadastralCode: rc,
+			Latitude:      lat,
+			Longitude:     lon,
+			Address:       address,
+			Area:          area,
+			NumPanels:     len(panels),
+			SolarInfo:     solarFarmResponse,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
 	}
-	areaPercentageQuery := query.Get("percentage")
-	if areaPercentageQuery == "" {
-		areaPercentageQuery = "100"
-	}
-
-	areaPercentage, err := strconv.ParseFloat(areaPercentageQuery, 64)
-	if err != nil && areaPercentage < 0 && areaPercentage > 100 {
-		http.Error(w, "Invalid 'area_percentage' parameter", http.StatusBadRequest)
-		return
-	}
-
-	budget := query.Get("budget")
-	if budget == "" {
-		budget = "med"
-	}
-
-	coord, err := getCoordinatesFromCadastralCode(rc)
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		http.Error(w, "Error getting coordinates", http.StatusBadRequest)
-		return
-	}
-
-	lon := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Geo.Xcen
-	lat := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Geo.Ycen
-	address := coord.ConsultaCPMRCResult.Coordenadas.Coord[0].Ldt
-
-	gmlFile, err := getGMLFromCatastro(rc)
-	if err != nil {
-		fmt.Println(err)
-		http.Error(w, "Error getting GML", http.StatusBadRequest)
-		return
-	}
-	polygon, err := getCoordinatesFromGML(gmlFile)
-	if err != nil {
-		fmt.Println(err)
-		http.Error(w, "Error getting coordinates from GML", http.StatusBadRequest)
-		return
-	}
-	area := polygonArea(polygon)
-	solarPanelType := GetSolarPanelByBudget(budget)
-
-	panelsTotal := rectanglesInsidePolygon(polygon, Rectangle{Width: solarPanelType.Width / 1000, Height: solarPanelType.Height / 1000})
-	panels := panelsTotal[:int(float64(len(panelsTotal))*areaPercentage/100)]
-
-	solarFarmResponse, err := GetSolarInfo(lat, lon, len(panels), solarPanelType)
-	if err != nil {
-		fmt.Println("Error getting solar info:", err)
-		http.Error(w, "Error getting solar info", http.StatusInternalServerError)
-		return
-	}
-
-	response := struct {
-		CadastralCode string            `json:"cadastral_code"`
-		Latitude      string            `json:"latitude"`
-		Longitude     string            `json:"longitude"`
-		Address       string            `json:"address"`
-		Area          float64           `json:"area"`
-		NumPanels     int               `json:"num_panels"`
-		SolarInfo     SolarFarmResponse `json:"solar_info"`
-	}{
-		CadastralCode: rc,
-		Latitude:      lat,
-		Longitude:     lon,
-		Address:       address,
-		Area:          area,
-		NumPanels:     len(panels),
-		SolarInfo:     solarFarmResponse,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
 }
