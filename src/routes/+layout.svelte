092f3786cd2259722d34937d5945842f04827d47
ecopunk <59477015+cesarempathy@users.noreply.github.com>
2023-04-01 11:31:23 +0200

    change icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,12 +12,12 @@
 		<span class="p-2 text-orange-400 text-xl font-semibold dark:text-white">
 			Solar
 		  </span>
-		<Sun class="ml-2" size="35" />
+		<Sun color="orange" class="ml-2" size="35" />
 		
 	</NavBrand>
 	
 	<NavHamburger color="orange"  on:click={toggle} />
-	<NavUl activeClass="text-white bg-orange-700 md:bg-transparent md:text-orange-700 md:dark:text-white dark:bg-orange-600 md:dark:bg-transparent" nonActiveClass="text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" {hidden}>
+	<NavUl activeClass="text-white bg-orange-400 md:bg-transparent md:text-orange-400 md:dark:text-white dark:bg-orange-600 md:dark:bg-transparent" nonActiveClass="text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" {hidden}>
 		<NavLi color="orange" href="/" active={true}>Home</NavLi>
 		<NavLi color="orange" href="/about">About</NavLi>
 		<NavLi color="orange" href="/docs">Docs</NavLi>
